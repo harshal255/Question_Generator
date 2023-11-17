15fa8b09328a06d49abbc6755943fa21c4e1547f
harshal255 <200280107119@ldceahm.gujgov.edu.in>
2023-11-17 23:49:12 +0530

    question marks = 5 for every type question

--- data.js
+++ data.js
@@ -123,231 +123,231 @@ const data = [
         "subject": "Physics",
         "topic": "Quantum Mechanics",
         "difficulty": "Medium",
-        "marks": 10
+        "marks": 5
     },
     {
         "question": "Name three key events of the Renaissance period.",
         "subject": "History",
         "topic": "Renaissance",
         "difficulty": "Medium",
-        "marks": 10
+        "marks": 5
     },
     {
         "question": "Describe the structure of an atom.",
         "subject": "Science",
         "topic": "Atomic Structure",
         "difficulty": "Medium",
-        "marks": 10
+        "marks": 5
     },
     {
         "question": "Explain the working principle of a neural network.",
         "subject": "Technology",
         "topic": "Artificial Intelligence",
         "difficulty": "Medium",
-        "marks": 10
+        "marks": 5
     },
     {
         "question": "Discuss the impact of climate change on biodiversity.",
         "subject": "Environmental Science",
         "topic": "Climate Change",
         "difficulty": "Medium",
-        "marks": 10
+        "marks": 5
     },
     {
         "question": "What are the major functions of the human digestive system?",
         "subject": "Biology",
         "topic": "Human Anatomy",
         "difficulty": "Medium",
-        "marks": 10
+        "marks": 5
     },
     {
         "question": "Explain the process of cellular respiration.",
         "subject": "Biology",
         "topic": "Cell Biology",
         "difficulty": "Medium",
-        "marks": 10
+        "marks": 5
     },
     {
         "question": "Compare and contrast mitosis and meiosis.",
         "subject": "Biology",
         "topic": "Cell Division",
         "difficulty": "Medium",
-        "marks": 10
+        "marks": 5
     },
     {
         "question": "What are the key principles of democratic governance?",
         "subject": "Political Science",
         "topic": "Democracy",
         "difficulty": "Medium",
-        "marks": 10
+        "marks": 5
     },
     {
         "question": "Explain the concept of supply and demand in economics.",
         "subject": "Economics",
         "topic": "Microeconomics",
         "difficulty": "Medium",
-        "marks": 10
+        "marks": 5
     },
     {
         "question": "Discuss the causes and consequences of the Industrial Revolution.",
         "subject": "History",
         "topic": "Industrial Revolution",
         "difficulty": "Medium",
-        "marks": 10
+        "marks": 5
     },
     {
         "question": "What are the fundamental principles of coding in Python?",
         "subject": "Programming",
         "topic": "Python",
         "difficulty": "Medium",
-        "marks": 10
+        "marks": 5
     },
     {
         "question": "Explain the process of evolution by natural selection.",
         "subject": "Biology",
         "topic": "Evolution",
         "difficulty": "Medium",
-        "marks": 10
+        "marks": 5
     },
     {
         "question": "Discuss the major themes in Shakespeare's sonnets.",
         "subject": "Literature",
         "topic": "Shakespearean Sonnets",
         "difficulty": "Medium",
-        "marks": 10
+        "marks": 5
     },
     {
         "question": "Explain the principles of object-oriented programming.",
         "subject": "Programming",
         "topic": "OOP",
         "difficulty": "Medium",
-        "marks": 10
+        "marks": 5
     },
     {
         "question": "Discuss the impact of globalization on culture.",
         "subject": "Sociology",
         "topic": "Globalization",
         "difficulty": "Medium",
-        "marks": 10
+        "marks": 5
     },
     {
         "question": "What are the key components of a persuasive essay?",
         "subject": "English",
         "topic": "Writing",
         "difficulty": "Medium",
-        "marks": 10
+        "marks": 5
     },
     {
         "question": "Derive the Schrödinger equation in quantum mechanics.",
         "subject": "Physics",
         "topic": "Quantum Mechanics",
         "difficulty": "Hard",
-        "marks": 15
+        "marks": 5
     },
     {
         "question": "Discuss the geopolitical implications of the Cold War.",
         "subject": "History",
         "topic": "Cold War",
         "difficulty": "Hard",
-        "marks": 15
+        "marks": 5
     },
     {
         "question": "Explain the process of protein folding and its significance.",
         "subject": "Biology",
         "topic": "Molecular Biology",
         "difficulty": "Hard",
-        "marks": 15
+        "marks": 5
     },
     {
         "question": "Implement a deep learning model for image recognition.",
         "subject": "Technology",
         "topic": "Deep Learning",
         "difficulty": "Hard",
-        "marks": 15
+        "marks": 5
     },
     {
         "question": "Analyze the economic impact of a global pandemic.",
         "subject": "Economics",
         "topic": "Global Economy",
         "difficulty": "Hard",
-        "marks": 15
+        "marks": 5
     },
     {
         "question": "Explain the mathematical basis of general relativity.",
         "subject": "Physics",
         "topic": "General Relativity",
         "difficulty": "Hard",
-        "marks": 15
+        "marks": 5
     },
     {
         "question": "Discuss the ethical implications of gene editing technology.",
         "subject": "Ethics",
         "topic": "Genetic Engineering",
         "difficulty": "Hard",
-        "marks": 15
+        "marks": 5
     },
     {
         "question": "Develop a secure blockchain-based voting system.",
         "subject": "Technology",
         "topic": "Blockchain",
         "difficulty": "Hard",
-        "marks": 15
+        "marks": 5
     },
     {
         "question": "Analyze the social impact of artificial intelligence on employment.",
         "subject": "Sociology",
         "topic": "AI and Employment",
         "difficulty": "Hard",
-        "marks": 15
+        "marks": 5
     },
     {
         "question": "Explain the mathematical principles behind quantum computing.",
         "subject": "Technology",
         "topic": "Quantum Computing",
         "difficulty": "Hard",
-        "marks": 15
+        "marks": 5
     },
     {
         "question": "Discuss the historical development of computer programming languages.",
         "subject": "Programming",
         "topic": "Programming Languages",
         "difficulty": "Hard",
-        "marks": 15
+        "marks": 5
     },
     {
         "question": "Analyze the impact of colonialism on African societies.",
         "subject": "History",
         "topic": "Colonialism",
         "difficulty": "Hard",
-        "marks": 15
+        "marks": 5
     },
     {
         "question": "Explain the principles of algorithmic trading in financial markets.",
         "subject": "Finance",
         "topic": "Algorithmic Trading",
         "difficulty": "Hard",
-        "marks": 15
+        "marks": 5
     },
     {
         "question": "Discuss the ethical considerations in artificial intelligence research.",
         "subject": "Ethics",
         "topic": "AI Ethics",
         "difficulty": "Hard",
-        "marks": 15
+        "marks": 5
     },
     {
         "question": "Analyze the impact of climate change on global food security.",
         "subject": "Environmental Science",
         "topic": "Climate Change",
         "difficulty": "Hard",
-        "marks": 15
+        "marks": 5
     },
     {
         "question": "Explain the principles of dark matter in astrophysics.",
         "subject": "Physics",
         "topic": "Astrophysics",
         "difficulty": "Hard",
-        "marks": 15
+        "marks": 5
     }
 ];
 
